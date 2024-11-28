42356c62ac756d181b2051f71725ba82d4b87e1c
emily-slalom <emily.lynn@slalom.com>
2024-11-27 23:50:53 -0600

    Updates for a new method of using postgressql that doesn't require root privileges on Binder

--- bootcamp/materials/1-dimensional-data-modeling/scripts/init-db.sh
+++ bootcamp/materials/1-dimensional-data-modeling/scripts/init-db.sh
@@ -8,15 +8,15 @@ pg_restore \
     --no-privileges \
     -U $POSTGRES_USER \
     -d $POSTGRES_DB \
-    /docker-entrypoint-initdb.d/data.dump
+    bootcamp/materials/1-dimensional-data-modeling/data.dump
 
 # Check if the path is a directory using the -d flag and
 #  there are SQL files in the directory using the -f command
 #   (the [] brackets are used for conditional expressions)
-if [ -d /docker-entrypoint-initdb.d/homework ]; then
+if [ -d bootcamp/materials/1-dimensional-data-modeling/homework ]; then
   echo "[SUCCESS]: Located homework directory"
   # Run any additional initialization scripts
-    for f in /docker-entrypoint-initdb.d/homework/*.sql; do
+    for f in bootcamp/materials/1-dimensional-data-modeling/homework/*.sql; do
       if [ -f "$f" ]; then
         echo "[SUCCESS] Running SQL file: $f"
         psql -U $POSTGRES_USER -d $POSTGRES_DB -f $f
@@ -25,5 +25,5 @@ if [ -d /docker-entrypoint-initdb.d/homework ]; then
       fi
     done
 else
-    echo "[ERROR] Directory not found: /docker-entrypoint-initdb.d/homework/"
+    echo "[ERROR] Directory not found: bootcamp/materials/1-dimensional-data-modeling/homework/"
 fi
